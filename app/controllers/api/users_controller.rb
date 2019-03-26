047ec062a063318c4c505992e39ba5179e63f9f5
rmunoz1994 <raymond_munoz@alumni.brown.edu>
2019-03-26 15:30:35 -0400

    Start user auth

---
+++ app/controllers/api/users_controller.rb
@@ -0,0 +1,24 @@
+class Api::UsersController < ApplicationController
+
+    def index
+        @users = User.all
+    end
+
+    def create
+        @user = User.new(user_params)
+        if @user.save
+            login!(@user)
+            render :show
+        else
+            render json: @user.errors.full_messages, status: 402
+        end
+    end
+
+    def show
+        @user = User.find(params[:id])
+    end
+
+    def user_params
+        params.require(:user).permit(:username, :email, :password, :profile_img_url)
+    end
+end
