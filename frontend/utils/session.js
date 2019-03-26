164acfa5e5676a149d01e80d08979494db03b380
rmunoz1994 <raymond_munoz@alumni.brown.edu>
2019-03-26 16:36:04 -0400

    Render Youtoob

---
+++ frontend/utils/session.js
@@ -0,0 +1,15 @@
+export const postUser = user => (
+    $.ajax({
+        method: 'POST',
+        url: 'api/users',
+        data: { user }
+    })
+);
+
+export const postSession = user => (
+    $.ajax({
+        method: 'POST',
+        url: 'api/session',
+        data: { user }
+    })
+);
