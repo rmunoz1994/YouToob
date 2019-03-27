5f4090530dd6b741b1e8611aacac128f36d51db9
App Academy Student <aa@Apps-Mac-mini.local>
2019-03-27 11:35:35 -0400

    Implement login

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import NavBarContainer from './nav_bar/nav_bar_container';
 import SignupContainer from './session/signup_container';
+import LoginContainer from './session/login_container';
 import { Route } from 'react-router-dom';
 import { AuthRoute, ProtectedRoute } from '../utils/route_utils';
 
@@ -8,6 +9,7 @@ const App = () => (
     <div>
         <Route path="/" component={ NavBarContainer } />
         <AuthRoute path="/signup" component={ SignupContainer }/>
+        <AuthRoute path="/login" component={ LoginContainer } />
     </div>
 );
 
---
+++ frontend/components/session/login.jsx
@@ -0,0 +1,61 @@
+import React from 'react';
+
+class Login extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            username: '',
+            email: '',
+            password: ''
+        };
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleInput(type) {
+        return (e) => {
+            this.setState({ [type]: e.target.value });
+        };
+    }
+
+    handleSubmit(e) {
+        e.preventDefault();
+        this.props.login(this.state)
+            .then(() => this.props.history.push('/'));
+    }
+
+    render() {
+        return (
+            <div className="session-form">
+                <h2>Sign in</h2>
+                to continue to YouToob
+                <br/>
+                <form onSubmit={this.handleSubmit}>
+                    <label>Username:</label>
+                    <input
+                        type="text"
+                        value={this.state.username}
+                        onChange={this.handleInput("username")}
+                    />
+                    <br />
+                    <label>Email:</label>
+                    <input
+                        type="text"
+                        value={this.state.email}
+                        onChange={this.handleInput("email")}
+                    />
+                    <br />
+                    <label>Password:</label>
+                    <input
+                        type="password"
+                        value={this.state.password}
+                        onChange={this.handleInput("password")}
+                    />
+                    <br />
+                    <input type="submit" value="Submit" />
+                </form>
+            </div>
+        )
+    }
+}
+
+export default Login;
