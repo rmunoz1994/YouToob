88e125d814ec9232e3234420985d7fb764a02140
Ray Munoz <Raymond_Munoz@alumni.brown.edu>
2019-08-19 11:45:12 -0400

    Start channel model

---
+++ app/models/channel.rb
@@ -0,0 +1,25 @@
+# == Schema Information
+#
+# Table name: channels
+#
+#  id          :bigint(8)        not null, primary key
+#  name        :string           not null
+#  description :text
+#  creator_id  :integer          not null
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
+
+class Channel < ApplicationRecord 
+
+    validates :name, presence: true, uniqueness: { scope: :creator_id, case_sensitive: false }
+
+    belongs_to :creator,
+        class_name: :User,
+        primary_key: :id,
+        foreign_key: :creator_id
+
+    has_many :videos, dependent: :destroy,
+
+
+end
