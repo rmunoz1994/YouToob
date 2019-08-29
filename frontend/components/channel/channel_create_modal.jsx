7e40e6b7f8b1e1ba39a70f9ed012d80d8037a2a3
Ray Munoz <Raymond_Munoz@alumni.brown.edu>
2019-08-29 17:23:15 -0400

    Modal completed

--- frontend/components/channel/channel_create_modal.jsx
+++ frontend/components/channel/channel_create_modal.jsx
@@ -5,22 +5,32 @@ class ChannelCreateModal extends React.Component {
 
     constructor(props) {
         super(props);
+        this.redirectToIndex = this.redirectToIndex.bind(this);
+    }
+
+    redirectToIndex() {
+        this.props.history.push('/');
+        this.props.closeModal();
     }
 
     render() {
         return (
             <div id="channel-create-container">
-                <header>Use YouToob as...</header>
+                <header>
+                    <h2>
+                        Use YouToob as...
+                    </h2>
+                </header>
                 <section>
                     <img src={window.newChannelPhoto} />
                     <form action="">
-                        <input className="modal-input" type="text"/>
-                        <input className="modal-input" type="text"/>
+                        <input className="modal-input" type="text" placeholder="Channel Name"/>
+                        <input className="modal-input" type="text" placeholder="Description" />
                     </form>
                 </section>
                 <footer>
-                    <button>CANCEL</button>
-                    <button>CREATE CHANNEL</button>
+                    <button className="cancel-channel-btn" onClick={this.redirectToIndex}>CANCEL</button>
+                    <button id="create-channel-btn">CREATE CHANNEL</button>
                 </footer>
             </div>
         );
